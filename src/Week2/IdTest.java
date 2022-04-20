f108f9308fe04c6b2664db0a7073b6f1a4635419
SohnSooKyoung <sksohn01@naver.com>
2022-04-20 20:39:03 +0900

    2022-04-20-laptop

--- src/Week2/IdTest.java
+++ src/Week2/IdTest.java
@@ -2,8 +2,8 @@ package Week2;
 
 class IdTest {
     public static void main(String[] args) {
-        String 이름 = "손수경"; // id : 한글과 영어 모두 사용할 수 있다. 그렇지만 한글은 가능하면 쓰지 말자.
+        String name2 = "손수경"; // id : 한글과 영어 모두 사용할 수 있다. 그렇지만 한글은 가능하면 쓰지 말자.
         String name = "Sohn Soo Kyoung";
-        System.out.println("안녕하세요 " + 이름 + "님" + "(" + name + ")");
+        System.out.println("안녕하세요 " + name2 + "님" + "(" + name + ")");
     }
 }
